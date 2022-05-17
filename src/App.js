c4f33f18e3132591c72dafb3a06aa2c4a0dfcda6
lumato92 <55516181+lumato92@users.noreply.github.com>
2022-05-17 20:23:43 -0300

    primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
+import Boton from './components/Boton';
+import BotonClear from './components/BotonClear';
+import Pantalla from './components/Pantalla';
 
 function App() {
+
+  const [input, setInput] = useState('')
+
+// Funcion que concatena los valores que se muestran en la pantalla
+
+  const agregarInput = val =>{
+    setInput(input + val);
+  };
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Calculadora React</h1>
       </header>
+
+      <div className='main-container'>
+
+          <Pantalla input={input}/>
+        <div className='fila'>
+
+ { /* manejarClic es un prop que utilizo para mandar una funcion como parametro  */}
+          <Boton manejarClic={agregarInput}>7</Boton>
+          <Boton manejarClic={agregarInput}>8</Boton>
+          <Boton manejarClic={agregarInput}>9</Boton>
+          <Boton manejarClic={agregarInput}>x</Boton>
+          </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>4</Boton>
+          <Boton manejarClic={agregarInput}>5</Boton>
+          <Boton manejarClic={agregarInput}>6</Boton>
+          <Boton  manejarClic={agregarInput}>-</Boton>
+          
+          </div>
+        <div className='fila'>
+          <Boton manejarClic={agregarInput}>1</Boton>
+          <Boton manejarClic={agregarInput}>2</Boton>
+          <Boton manejarClic={agregarInput}>3</Boton>
+          <Boton manejarClic={agregarInput}>+</Boton>
+          </div>
+        <div className='fila'>
+          <Boton  manejarClic={agregarInput}>=</Boton>
+          <Boton  manejarClic={agregarInput}>0</Boton>
+          <Boton  manejarClic={agregarInput}>.</Boton>
+          <Boton  manejarClic={agregarInput}>/</Boton>
+          </div>
+        <div className='fila'></div>
+          <BotonClear manejarClic={()=>setInput('')}>Clear</BotonClear>
+
+      </div>
+
     </div>
+
+    
   );
 }
 
---
+++ src/components/Pantalla.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../stylesheets/Pantalla.css"
+
+const Pantalla = ({input}) => (
+    <div className="input">
+
+        {input}
+
+    </div>
+)
+
+
+export default Pantalla;
